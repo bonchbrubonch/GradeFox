fd5e0a6dfb2addd17ed07c77ca5019deb97af9b4
Artem <nikolloteslla@gmail.com>
2024-07-22 11:20:15 +0300

    start project

---
+++ app/js/main.js
@@ -0,0 +1,16 @@
+$(function () {
+  /*start header====*/
+  $(window).scroll(function () {
+    var header = $("header");
+    if ($(this).scrollTop() > 0) {
+      header.addClass("sticky");
+    } else {
+      header.removeClass("sticky");
+    }
+  });
+  /*end header====*/
+
+
+
+
+})
